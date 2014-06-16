7e5ad820ba9d7c9cb77e071b85b2d2da946165bf
marsa746079 <marsa746079@gmail.com>
2014-06-17 03:04:14 +0900

    適当に進めた

--- js/ierukana.js
+++ js/ierukana.js
@@ -1,7 +1,17 @@
 
 ImasCg.Ierukana = function () {
 
+	var answer_mode_enum = {
+		full_name: 1,
+		full_name_kana: 2,
+		first_name: 4,
+		first_name_kana: 8,
+		last_name: 16,
+		last_name_kana: 32,
+	};
+
 	var jsonData = null;
+	var answer_mode = null;
 
 	/**
 	 * SessionStorageから値を取得する。
@@ -30,27 +40,64 @@ ImasCg.Ierukana = function () {
 
 	/**
 	 * 入力した名前がヒットしているか調べる
+	 * @param {string} str 入力したテキスト
+	 * @param {number} mode answer_mode_enum のうち必要な値をORした値
+	 * @return ヒットしたアイドルのID
 	 */
 	var isMatchName = function(str, mode) {
-
+		$.each(jsonData, function(index, data) {
+			$.each(answer_mode_enum, function(key, value) {
+				if (mode & value) {
+					if (data[key] === str)
+						return data.id;
+				}
+			});
+		});
+		return 0;
 	};
 
 	return {
 
+		/**
+		 * 初期化処理
+		 */
 		init: function () {
 			jsonData = getCache("imas-cg-ierukana");
 			if (jsonData) return;
 
 			$.get('data/idols.json').done(function(data) {
 				jsonData = $.parseJSON(data); 
-				setCache("nations", data);
+				setCache("imas-cg-ierukana", data);
 			}).fail(function(errorData) {
 				// TODO エラー処理
 			});
 		},
 
-		try: function (name) {
+		/**
+		 * ゲーム開始ボタン押下時の処理
+		 */
+		game_start: function (mode) {
+			// タイマーカウント開始
+		},
 
-		}
+		/**
+		 * ゲーム終了ボタン押下時の処理
+		 */
+		game_end: function () {
+			// タイマーカウント終了
+		},
+
+		/**
+		 * 解答ボタン押下時の処理
+		 */
+		answer: function (name) {
+			var id = isMatchName(name, answer_mode);
+			if (id) {
+				// ヒットした時、該当アイドル名をオープン
+			} else {
+				// ヒットしない時は警告を出す？
+			}
+		},
+
 	};
 }();
